1b341df54a9ccc130b7a339c5f113a71e573e961
Collin <cjwischmeyer@gmail.com>
2024-05-05 13:06:05 -0400

    adding try import func

--- src/canhydro/import_options.py
+++ src/canhydro/import_options.py
@@ -1,22 +1,10 @@
-import __builtin__
 from types import ModuleType
 
-
-class DummyModule(ModuleType):
-    def __getattr__(self, key):
-        return None
-    __all__ = []   # support wildcard imports
-
-
-excluded_imports = ['geopandas']
-
-def tryimport(name, globals={}, locals={}, fromlist=[], level=-1):
+def _try_import(name, src):
     try:
-        if name in excluded_imports:
-            return DummyModule(name)
-        else:    
-            return realimport(name, globals, locals, fromlist, level)
+        name = None
+        import_statment = f'from src import name'
+        eval(import_statment)
+        return True 
     except ImportError:
-        return DummyModule(name)
-
-realimport, __builtin__.__import__ = __builtin__.__import__, tryimport
+        return False
