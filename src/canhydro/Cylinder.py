6df074b3615c4a493a68ebd4574a9dc53f36c561
Collin <cjwischmeyer@gmail.com>
2024-08-07 23:29:42 -0400

    Cleaning up cylinder class for readability

--- src/canhydro/Cylinder.py
+++ src/canhydro/Cylinder.py
@@ -26,6 +26,14 @@ NAME = "Cylinder"
 
 
 def create_cyl(arr: np.array):
+    """Creates a cylinder based off of an input array.
+        Enables user defined file structure via configuration yml file 
+
+    Args:
+        arr (np.array): 
+            Array containing attribute data for a single cylinder.
+            Attributes must be ordered as per qsm_cols in user_def_config.yml
+    """
     cols = qsm_cols
     attrs = {k: arr[v] for (k, v) in cols.items()}
     cyl = Cylinder(**attrs)
@@ -69,19 +77,21 @@ class Cylinder:
     is_divide: bool = False
 
     def __repr__(self):
+        """Defines a readable string to represent a cylinder object
+            *Utilized in tests to compare expected and actual results
+        """
         return f"Cylinder( cyl_id={self.cyl_id}, x={self.x}, y={self.y}, z={self.z}, radius={self.radius}, length={self.length}, branch_order={self.branch_order}, branch_id={self.branch_id}, volume={self.volume}, parent_id={self.parent_id}, reverse_branch_order={self.reverse_branch_order}, segment_id={self.segment_id}"
 
     def __eq__(self, other):
+        """Defines the minimum requirements for equality between two cylinders. 
+            Allows use of '=' for comparing cylinders
+        """
         return type(self) == type(other) and self.__repr__() == other.__repr__()
 
-    def calc_surface_area(self):
-        radius = self.radius
-        length = self.length
-        sa = 2 * np.pi * radius * (radius + length) - 2 * np.pi * radius * radius
-        return sa
-
     def create_from_list(self, attrs: list, columns=qsm_cols):
-        """creates a cylinder corrosponding to that defined by a given row of the qsm (attrs)"""
+        """Initializes remaining attributes based off of the 
+                attributes provided at object creation
+        """
 
         self.dx = self.x[1] - self.x[0]
         self.dy = self.y[1] - self.y[0]
@@ -100,7 +110,10 @@ class Cylinder:
                 np.array([self.y[1], self.z[1], self.x[1]]),
             ],
         }
-        self.surface_area = self.calc_surface_area()
+        radius = self.radius
+        length = self.length
+        self.surface_area = 2 * np.pi * radius * (radius + length) - 2 * np.pi * radius * radius
+
         self.sa_to_vol = 0 if self.volume == 0 else self.surface_area / self.volume
         run = np.sqrt(self.dx**2 + self.dy**2)
         self.angle = (
@@ -109,18 +122,51 @@ class Cylinder:
             else np.arctan(self.dz / np.sqrt(self.dx**2 + self.dy**2))
         )
         self.xy_area = 0
-        log.debug(str(self.__repr__()))
 
     def get_projection(self, plane="XY"):
+        """Calculates the projection of the cylinder on the requested plane 
+
+        Args:
+            self (Cylinder): 
+                Cylinder to be projected 
+            plane (str, optional): 
+                Plane on which to project the cylinder. Defaults to "XY".
+        
+        Sets Attributes:
+                - projected_data (dict)
+                    - key->value:
+                        - polygon (shapely.Polygon):
+                            represents the shape formed by projecting 
+                            the cylinder (self) onto the given plane
+                        - base_vector (list[float]): 
+                            cylinder vector *after projection 
+                            (oriented away from cylinder base)
+                        - anti_vector (list[float]):
+                            cylinder vector *after projection 
+                            (oriented towards cylinder base)
+                        - angle (float):
+                            angle (in radians) of cylinder vector with 
+                            given axis. Between -pi/2 and pi/2.
+                        - area (float) - area of polygon (see above)
+                - xy_area: float 
+                    - polygon formed by projecting the cylinder
+                        onto the XY plane
+
+        Returns:
+            (shapely.Polygon): projected_data[polygon]
+
+        """
         if plane == "XY":
             magnitude = [self.dx, self.dy, self.dz]
-            vector = [np.transpose(self.x), np.transpose(self.y), np.transpose(self.z)]
+            ranges = [self.x, self.y, self.z]
         elif plane == "XZ":
             magnitude = [self.dx, self.dz, self.dy]
-            vector = [np.transpose(self.x), np.transpose(self.z), np.transpose(self.y)]
+            ranges = [self.x, self.z, self.y]
         else:
             magnitude = [self.dy, self.dz, self.dx]
-            vector = [np.transpose(self.y), np.transpose(self.z), np.transpose(self.x)]
+            ranges = [self.y, self.z, self.x]
+
+        vector = list(map(np.transpose, ranges))
 
         projection = get_projection(vector, magnitude, self.radius)
         self.projected_data[plane] = projection
@@ -129,5 +175,9 @@ class Cylinder:
         return projection["polygon"]
 
     def draw(self, plane: str = "XY"):
+        """A wrapper around the draw_cyl function allowing 
+            more readable code for drawing a single cylinder 
+                - e.g. for some Cylider 'cyl'
+                    cyl.draw() rather than geometry.draw_cyls([cyl]) """
         poly = self.projected_data[plane]["polygon"]
         draw_cyls([poly])
